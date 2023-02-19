cb3ab576d1a7d912623a8d536e39ced5cb16a877
loochoonboon <looboon@outlook.com>
2023-02-19 19:30:03 +0800

    fix(key): prevent reregister key

--- x/cosmosmessenger/keeper/msg_server_register_wallet_key.go
+++ x/cosmosmessenger/keeper/msg_server_register_wallet_key.go
@@ -16,6 +16,10 @@ func (k msgServer) RegisterWalletKey(goCtx context.Context, msg *types.MsgRegist
 		return nil, status.Error(codes.InvalidArgument, "please provide a valid base64 encoded RSA public keys")
 	}
 
+	if _, found := k.GetEncryptionKey(ctx, msg.GetCreator()); found {
+		return nil, status.Error(codes.AlreadyExists, "public key already exist for user")
+	}
+
 	k.StoreEncryptionKey(ctx, types.EncryptKey{
 		PubKey: msg.GetPubkey(),
 	}, msg.GetCreator())
--- x/cosmosmessenger/keeper/msg_server_register_wallet_key_test.go
+++ x/cosmosmessenger/keeper/msg_server_register_wallet_key_test.go
@@ -36,6 +36,27 @@ func TestMsgServer_RegisterWalletKey(t *testing.T) {
 			Pubkey:  "example",
 		})
 		// assert
-		require.Error(t, err)
+		require.Equal(t, "rpc error: code = InvalidArgument desc = please provide a valid base64 encoded RSA public keys", err.Error())
+	})
+
+	t.Run("should fail to register new pub key if key already exists", func(t *testing.T) {
+		t.Parallel()
+		// arrange
+		msgSvr, _, ctx := setupMsgServer(t)
+		// act
+		resp, err := msgSvr.RegisterWalletKey(ctx, &types.MsgRegisterWalletKey{
+			Creator: creatorAddr,
+			Pubkey:  pubKeyStr,
+		})
+		require.NoError(t, err)
+		require.True(t, resp.GetSuccess())
+		// register second time
+		_, pubKeyStrSecond := getEncryptionKeys(t)
+		_, err = msgSvr.RegisterWalletKey(ctx, &types.MsgRegisterWalletKey{
+			Creator: creatorAddr,
+			Pubkey:  pubKeyStrSecond,
+		})
+		// assert
+		require.Equal(t, "rpc error: code = AlreadyExists desc = public key already exist for user", err.Error())
 	})
 }
